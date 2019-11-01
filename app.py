170c37caf3c0eebba15a0e882681486f05d26050
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-11-01 12:39:37 -0700

    Add code for precipitation route in api

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ import pandas as pd
 # Set up engine
 engine = create_engine("sqlite:///hawaii.sqlite")
 
-Base = automap_base
+Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+# Flask Routes and code
 
 app = Flask(__name__)
 
@@ -25,11 +27,30 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return(
+        f"Hawaii Weather API<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.o/precipitation<br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Precitpitation Data for the Last Year"""
+    session = Session(engine)
+    last_day = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date.desc()).first()[0]
+    last_year = str(dt.datetime.strptime(last_day, "%Y-%m-%d") - dt.timedelta(days=365))
+
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= last_year, Measurement.date <= last_day).\
+        order_by(Measurement.date).all()
+    prcp_dict = {date: prcp for date, prcp in precipitation}
+    return jsonify(prcp_dict)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
