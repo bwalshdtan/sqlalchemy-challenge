41c0bc2e56fef4e97abea5af709b558eb836a62f
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-11-01 11:25:44 -0700

    Set up python code for app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+#Dependencies
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+# Set up engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.o/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
